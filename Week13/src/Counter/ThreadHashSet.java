ba62e673c93435f1c60a5c7ff700998193aa9436
u1359468 <u1359468@CW301-10.AD.HUD.AC.UK>
2016-01-22 15:09:46 +0000

    ThreadHash set implementation of run set completed.

--- Week13/src/Counter/ThreadHashSet.java
+++ Week13/src/Counter/ThreadHashSet.java
@@ -3,4 +3,18 @@ package Counter;
 import java.util.HashSet;
 
 public class ThreadHashSet<T extends Thread> extends HashSet<T> implements ThreadSet<T>{
+
+	public void runSet() throws InterruptedException {
+		for(Thread thread : this)
+		{
+			thread.start();
+		}
+		for(Thread thread : this)
+		{
+			try{
+				thread.join();
+			}catch(InterruptedException e){};
+		}
+		
+	}
 }
